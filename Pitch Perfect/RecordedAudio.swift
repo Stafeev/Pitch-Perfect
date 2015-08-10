ad3c06e50ac8dd7753135e00ba9ffe21c96d96a8
Stafeev <dunlop300g@yandex.ru>
2015-08-10 09:44:16 -0700

    First commit

---
+++ Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,13 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Artem Stafeev on 5/31/15.
+//  Copyright (c) 2015 Artem Stafeev. All rights reserved.
+//
+
+import Foundation
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
